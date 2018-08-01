d08a05afaed7f1f0241473a515d067aa1f9d9c00
Hernan Razo <hernanrazo@gmail.com>
2018-08-01 14:49:52 -0500

    finished models

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 UCI Iris Classification
 ===
+
+https://www.kaggle.com/adityabhat24/iris-data-analysis-and-machine-learning-python  
+https://www.kaggle.com/uciml/iris/home  
+https://www.kaggle.com/ash316/ml-from-scratch-with-iris
--- iris.py
+++ iris.py
@@ -5,6 +5,8 @@ import seaborn as sns
 from matplotlib import pyplot as plt 
 from sklearn import metrics
 from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.neighbors import KNeighborsClassifier
 
@@ -21,8 +23,8 @@ print(' ')
 print(df.describe())
 print(' ')
 
-#view class distribution
-print(df.groupby('class').size())
+#view species distribution
+print(df.groupby('species').size())
 print(' ')
 
 #start visualizing and analyzing the data
@@ -68,29 +70,73 @@ plt.title('Petal-Width')
 df['petal-width'].hist(bins = 15)
 petalWidthHist.savefig(graph_folder_path + 'petalWidthHist.png')
 
-#make a violin plot of petal length based off class
-petalLengthClassViolin = plt.figure()
-plt.title('Petal-Length By Class')
-sns.violinplot(data = df, x = 'class', y = 'petal-length')
-petalLengthClassViolin.savefig(graph_folder_path + 'petalLengthClassViolin.png')
+#make a violin plot of petal length based off species
+petalLengthspeciesViolin = plt.figure()
+plt.title('Petal-Length By species')
+sns.violinplot(data = df, x = 'species', y = 'petal-length')
+petalLengthspeciesViolin.savefig(graph_folder_path + 'petalLengthspeciesViolin.png')
 
 #plot scatter plot matrix
 scatterMatrix = plt.figure()
 plt.title('Scatter Plot Matrix')
-sns.pairplot(df, hue = 'class')
+sns.pairplot(df, hue = 'species')
 scatterMatrix.savefig(graph_folder_path + 'scatterPlot.png')
 
+#split the dataset into a training section and testing section
+train, test = train_test_split(df, test_size = 0.3)
 
+#take data features and output for training and testing
+train_x = train[['sepal-length', 'sepal-width', 'petal-length', 'petal-width']]
+train_y = train['species']
 
+test_x = train[['sepal-length', 'sepal-width', 'petal-length', 'petal-width']]
+test_y = train['species']
 
+#double check everything came out correctly
+print(train.head)
+print(' ')
+print(test.head)
+print(' ')
 
+#finally start training models
+model = KNeighborsClassifier(n_neighbors = 3)
+model.fit(train_x, train_y)
+prediction = model.predict(test_x)
+print(metrics.accuracy_score(prediction, test_y))
+print(' ')
 
+#now test the same algorithm using petal and sepal data seperately
+petal = df[['petal-length', 'petal-width', 'species']]
+sepal = df[['sepal-length', 'sepal-width', 'species']]
 
+#split the data into a training and testing section again
+train_petal, test_petal = train_test_split(petal, test_size = 0.3, random_state = 0)
+train_petal_x = train_petal[['petal-length', 'petal-width']]
+train_petal_y = train_petal['species']
 
+test_petal_x = test_petal[['petal-length', 'petal-width']]
+test_petal_y = test_petal['species']
 
+train_sepal, test_sepal = train_test_split(sepal, test_size = 0.3, random_state = 0)
+train_sepal_x = train_sepal[['sepal-length', 'sepal-width']]
+train_sepal_y = train_sepal['species']
 
+test_sepal_x = test_sepal[['sepal-length', 'sepal-width']]
+test_sepal_y = test_sepal['species']
 
+#train algorithm with new data
+print('New training session:')
+#petals
+model = KNeighborsClassifier(n_neighbors = 3)
+model.fit(train_petal_x, train_petal_y)
+prediction = model.predict(test_petal_x)
+print('Petal prediction: ')
+print(metrics.accuracy_score(prediction, test_petal_y))
+print(' ')
 
-
-
-
+#sepals
+model = KNeighborsClassifier(n_neighbors = 3)
+model.fit(train_sepal_x, train_sepal_y)
+prediction = model.predict(test_sepal_x)
+print('Sepal prediction: ')
+print(metrics.accuracy_score(prediction, test_sepal_y))
